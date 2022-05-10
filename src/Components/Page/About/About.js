b7ec0be93fbefd7f2e5c35ea85426c744044333c
mdarafatyeasin <yeasinarafat54239@gmail.com>
2022-05-11 00:08:45 +0600

    basic jwt token componests are created

---
+++ src/Components/Page/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css'
+
+const About = () => {
+    return (
+        <div>
+            <h1>This is About page</h1>
+        </div>
+    );
+};
+
+
+export default About;
