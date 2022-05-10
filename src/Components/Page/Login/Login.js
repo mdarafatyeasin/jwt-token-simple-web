a3bf7a84dbdf9fc4277a04ceae2a5d2e90c5d751
mdarafatyeasin <yeasinarafat54239@gmail.com>
2022-05-11 00:49:24 +0600

    basic implement of login page

--- src/Components/Page/Login/Login.js
+++ src/Components/Page/Login/Login.js
@@ -4,7 +4,12 @@ import './Login.css'
 const Login = () => {
     return (
         <div>
-            <h1>This is Login page</h1>
+            <h1>Please Login page</h1>
+            <form className='input-field'>
+                <input className='input' type='password' placeholder= {'Enter your password'}></input> <br />
+                <input className='input' type='email' placeholder= {'Enter your email'}></input> <br />
+                <input className='input' type='submit' value="Login"></input>
+            </form>
         </div>
     );
 };
