58e368d92e2b3a627c64fc607bdc6e00ecc84242
mdarafatyeasin <yeasinarafat54239@gmail.com>
2022-05-11 00:38:19 +0600

    set all routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header/Header';
+import Home from './Components/Page/Home/Home' 
+import Order from './Components/Page/Order/Order' 
+import About from './Components/Page/About/About' 
+import Login from './Components/Page/Login/Login' 
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      
+
+      {/* navigation routers */}
+      <Routes>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/order' element={<Order></Order>}></Route>
+        <Route path='/About' element={<About></About>}></Route>
+        <Route path='/Login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
@@ -9,9 +10,10 @@ const Header = () => {
                 <Container>
                     <Navbar.Brand href="#home">JWT Token</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link as={Link} to="home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="Order">Order</Nav.Link>
+                        <Nav.Link as={Link} to="About">About</Nav.Link>
+                        <Nav.Link as={Link} to="Login">Login</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
