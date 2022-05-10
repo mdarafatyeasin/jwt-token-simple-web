46a209fe089c5bd87a39e18f0ea6ebc156fe9ea9
mdarafatyeasin <yeasinarafat54239@gmail.com>
2022-05-11 00:19:08 +0600

    created navbar in header component by react bootstrap

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">JWT Token</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </div>
     );
 };
