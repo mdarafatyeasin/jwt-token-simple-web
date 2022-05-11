d072a78af0d537ec571269bc5c3aca12adbe8e3a
mdarafatyeasin <yeasinarafat54239@gmail.com>
2022-05-11 19:54:51 +0600

    jwt token inplement sussessfully

--- src/Components/Page/Login/Login.js
+++ src/Components/Page/Login/Login.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
+
+    const navigate = useNavigate()
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+
+        fetch('http://localhost:5000/login', {
+            method: 'POST',
+            headers:{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({email, password})
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.success){
+                localStorage.setItem('accessToken', data.accessToken);
+                navigate('/order')
+
+            }
+            console.log(data)
+        })
+
+    }
     return (
         <div>
             <h1>Please Login page</h1>
-            <form className='input-field'>
-                <input className='input' type='password' placeholder= {'Enter your password'}></input> <br />
-                <input className='input' type='email' placeholder= {'Enter your email'}></input> <br />
+            <form onSubmit={handleLogin} className='input-field'>
+                <input className='input' type='email' name='email' placeholder= {'Enter your email'}></input> <br />
+                <input className='input' type='password' name='password' placeholder= {'Enter your password'}></input> <br />
                 <input className='input' type='submit' value="Login"></input>
             </form>
         </div>
--- src/Components/Page/Order/Order.js
+++ src/Components/Page/Order/Order.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Order.css'
 
 const Order = () => {
+    const [orders, setOrders] = useState([])
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        fetch('http://localhost:5000/order', {
+            method: 'GET',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if (res.status === 401 || res.status === 403) {
+                    navigate('/login')
+                }
+                console.log(res)
+                return res.json()
+            })
+            .then(data => {
+                console.log(data)
+                setOrders(data)
+            })
+    }, [])
+
+
     return (
         <div>
-            <h1>This is order page</h1>
+            <h1>This is order page {orders.length}</h1>
         </div>
     );
 };
